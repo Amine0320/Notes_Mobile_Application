449ebf12507634f4b85ac67d5b2123f32a694ab6
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2024-02-14 14:07:44 +0100

    LAST Wed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/firebase_options.dart';
 import 'package:notes_app/views/login_view.dart';
 import 'package:notes_app/views/register_view.dart';
+import 'package:notes_app/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -47,6 +49,17 @@ class HomePage extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         } else {
+          final user = FirebaseAuth.instance.currentUser;
+          if (user != null) {
+            if (user.emailVerified) {
+              print('Email is verified');
+            } else {
+              return const VerifyEmailView();
+            }
+          } else {
+            return const LoginView();
+          }
+          return const Text('Done');
           // OLD LOGIC
           // final user = FirebaseAuth.instance.currentUser;
           // // print(user);
@@ -57,9 +70,26 @@ class HomePage extends StatelessWidget {
           //   // Push the verify email view to the home screen
           // }
           // return const Text('Done');
-          return const LoginView();
         }
       },
+    );
+  }
+}
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+      ),
     );
   }
 }
