f39985ef59415a8d77687f2eee1caee738ec7702
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2024-02-26 22:46:11 +0100

    LAST

--- lib/contants/routes.dart
+++ lib/contants/routes.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/material.dart';
+
 const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
 const verifyEmailRoute = '/verify';
+
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+
+const double defaultPadding = 16.0;
