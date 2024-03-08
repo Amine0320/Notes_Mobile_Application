0882004d5d5f2350ebb5f86160e29ab97fc276d5
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2024-03-08 15:30:23 +0100

    EDIT

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/contants/routes.dart';
 import 'package:notes_app/exceptions/auth/auth_service.dart';
-import 'package:notes_app/views/login_view.dart';
+import 'package:notes_app/views/Authentification/login_view.dart';
+import 'package:notes_app/views/Authentification/register_view.dart';
+import 'package:notes_app/views/Authentification/verify_email_view.dart';
 import 'package:notes_app/views/notes/create_update_note_view.dart';
 import 'package:notes_app/views/notes/notes_view.dart';
-import 'package:notes_app/views/register_view.dart';
-import 'package:notes_app/views/verify_email_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
